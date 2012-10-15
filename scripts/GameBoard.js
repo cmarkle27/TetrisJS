32c5c3421f7ac557ab57b9f528c5e6d754193f1f
Chris Markle <markle976@gmail.com>
2012-10-15 13:35:51 -0400

    tidy and delete

--- scripts/GameBoard.js
+++ scripts/GameBoard.js
@@ -1,239 +1,239 @@
 define("GameBoard", ["Tetrimino", "Canvas"], function(Tetrimino, Canvas) {
 
-	// static vars
-	var boardWidth = 480;
-	var boardHeight = 600;
-	var boardCanvas, pieceCanvas;
+  // static vars
+  var boardWidth = 480;
+  var boardHeight = 600;
+  var boardCanvas, pieceCanvas;
 
-	// --------------------------------------------------------------------
-	// GameBoard constructor
-	// --------------------------------------------------------------------
+  // --------------------------------------------------------------------
+  // GameBoard constructor
+  // --------------------------------------------------------------------
 
   var GameBoard = function(boardContext, pieceContext) {
-		this.speed = 700;
-		this.paused = true;
-		this.blockStack = [];
-		this.currentPiece = "";
-		boardCanvas = new Canvas(boardContext, boardWidth, boardHeight);
-		pieceCanvas = new Canvas(pieceContext, boardWidth, boardHeight);
-		this.loop();
-	};
+    this.speed = 700;
+    this.paused = true;
+    this.blockStack = [];
+    this.currentPiece = "";
+    boardCanvas = new Canvas(boardContext, boardWidth, boardHeight);
+    pieceCanvas = new Canvas(pieceContext, boardWidth, boardHeight);
+    this.loop();
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.startGame = function() {
-		boardCanvas.clear();
-		pieceCanvas.clear();
-		this.paused = false;
-	};
+  GameBoard.prototype.startGame = function() {
+    boardCanvas.clear();
+    pieceCanvas.clear();
+    this.paused = false;
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.dropPiece = function(action) {
-		this.speed = (action === "start") ? 70 : 700;
-	};
+  GameBoard.prototype.dropPiece = function(action) {
+    this.speed = (action === "start") ? 70 : 700;
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.createPiece = function() {
-		// we really need to create two pieces, so that we can show the user whats on deck!!!
-		this.currentPiece = new Tetrimino();
-		this.drawCurrentPiece();
-	};
+  GameBoard.prototype.createPiece = function() {
+    // we really need to create two pieces, so that we can show the user whats on deck!!!
+    this.currentPiece = new Tetrimino();
+    this.drawCurrentPiece();
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.drawCurrentPiece = function() {
+  GameBoard.prototype.drawCurrentPiece = function() {
 
-		var blocks = this.currentPiece.getBlocks();
-		var tileSize = this.currentPiece.tileSize;
-		var tileColor = this.currentPiece.tileColor;
+    var blocks = this.currentPiece.getBlocks();
+    var tileSize = this.currentPiece.tileSize;
+    var tileColor = this.currentPiece.tileColor;
 
-		pieceCanvas.clear();
+    pieceCanvas.clear();
 
-		for (var i = 0; i < 4; i++) {
-			pieceCanvas.renderTile({
-				x: blocks.x[i],
-				y: blocks.y[i],
-				color: tileColor,
-				size: tileSize
-			});
+    for (var i = 0; i < 4; i++) {
+      pieceCanvas.renderTile({
+        x: blocks.x[i],
+        y: blocks.y[i],
+        color: tileColor,
+        size: tileSize
+      });
 
-		}
+    }
 
-	};
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.movePiece = function(direction) {
+  GameBoard.prototype.movePiece = function(direction) {
 
-		var piecePosition = this.currentPiece.position;
-		var pieceDepth = this.currentPiece.depth;
-		var pieceOrientation = this.currentPiece.orientation;
-		var pieceRotations = this.currentPiece.getShape().length;
-		var canMove = false;
+    var piecePosition = this.currentPiece.position;
+    var pieceDepth = this.currentPiece.depth;
+    var pieceOrientation = this.currentPiece.orientation;
+    var pieceRotations = this.currentPiece.getShape().length;
+    var canMove = false;
 
-		if (direction === "left") {
-			piecePosition -= 1;
-		} else if (direction === "right") {
-			piecePosition += 1;
-		} else if (direction === "down") {
-			pieceDepth += 1;
-		} else if (direction === "clockwise") {
-			pieceOrientation += 1;
-		} else if (direction === "counterclockwise") {
-			pieceOrientation -= 1;
-		}
+    if (direction === "left") {
+      piecePosition -= 1;
+    } else if (direction === "right") {
+      piecePosition += 1;
+    } else if (direction === "down") {
+      pieceDepth += 1;
+    } else if (direction === "clockwise") {
+      pieceOrientation += 1;
+    } else if (direction === "counterclockwise") {
+      pieceOrientation -= 1;
+    }
 
-		// check for overrotatation
-		if (pieceOrientation === pieceRotations) {
-			pieceOrientation = 0;
-		} else if (pieceOrientation < 0) {
-			pieceOrientation = (pieceRotations -1);
-		}
+    // check for overrotatation
+    if (pieceOrientation === pieceRotations) {
+      pieceOrientation = 0;
+    } else if (pieceOrientation < 0) {
+      pieceOrientation = (pieceRotations -1);
+    }
 
-		// check our new location
-		canMove = this.checkMove(piecePosition, pieceDepth, pieceOrientation);
+    // check our new location
+    canMove = this.checkMove(piecePosition, pieceDepth, pieceOrientation);
 
-		if (canMove) {
-			this.currentPiece.position = piecePosition;
-			this.currentPiece.depth = pieceDepth;
-			this.currentPiece.orientation = pieceOrientation;
-			this.drawCurrentPiece();
-		} else {
-			if (direction === "down") {
-				this.addPiece(this.currentPiece);
-				this.currentPiece = null;
-				this.createPiece();
-			}
-		}
+    if (canMove) {
+      this.currentPiece.position = piecePosition;
+      this.currentPiece.depth = pieceDepth;
+      this.currentPiece.orientation = pieceOrientation;
+      this.drawCurrentPiece();
+    } else {
+      if (direction === "down") {
+        this.addPiece(this.currentPiece);
+        this.currentPiece = null;
+        this.createPiece();
+      }
+    }
 
-	};
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.checkMove = function(position, depth, orientation) {
+  GameBoard.prototype.checkMove = function(position, depth, orientation) {
 
-		var hits = 0;
-		var shape = this.currentPiece.getShape();
-		var x = shape[orientation].x;
-		var y = shape[orientation].y;
-		var tileSize = this.currentPiece.tileSize;
-		var xCoord, yCoord;
+    var hits = 0;
+    var shape = this.currentPiece.getShape();
+    var x = shape[orientation].x;
+    var y = shape[orientation].y;
+    var tileSize = this.currentPiece.tileSize;
+    var xCoord, yCoord;
 
-		for (i = 0; i < 4; i++) {
-			xCoord = x[i] + position;
-			yCoord = y[i] + depth;
+    for (i = 0; i < 4; i++) {
+      xCoord = x[i] + position;
+      yCoord = y[i] + depth;
 
-			// hit wall
-			if (xCoord === (boardWidth/tileSize) || xCoord < 0) {
-				hits += 1;
-			}
+      // hit wall
+      if (xCoord === (boardWidth/tileSize) || xCoord < 0) {
+        hits += 1;
+      }
 
-			// hit floor
-			if (yCoord === (boardHeight/tileSize)) {
-				hits += 1;
-			}
+      // hit floor
+      if (yCoord === (boardHeight/tileSize)) {
+        hits += 1;
+      }
 
-			console.log(yCoord);
+      console.log(yCoord);
 
-			// hit roof
-/*			if (yCoord === (boardHeight/tileSize)) {
-				hits += 1;
-			}*/
+      // hit roof
+/*      if (yCoord === (boardHeight/tileSize)) {
+        hits += 1;
+      }*/
 
-			// hit blocks
-			if (this.blockCheck(xCoord, yCoord)) {
-				hits += 1;
-			}
+      // hit blocks
+      if (this.blockCheck(xCoord, yCoord)) {
+        hits += 1;
+      }
 
-		}
+    }
 
-		if (hits > 0) {
-			return false;
-		}
+    if (hits > 0) {
+      return false;
+    }
 
-		return true;
-	};
+    return true;
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.addPiece = function(piece) {
+  GameBoard.prototype.addPiece = function(piece) {
 
-		var blocks = piece.getBlocks();
-		var tileSize = piece.tileSize;
-		var tileColor = piece.tileColor;
-		var blockStack = this.blockStack;
-		var overflows = 0;
+    var blocks = piece.getBlocks();
+    var tileSize = piece.tileSize;
+    var tileColor = piece.tileColor;
+    var blockStack = this.blockStack;
+    var overflows = 0;
 
-		for (var i = 4; i--;) {
-			this.blockStack.push({
-				x: blocks.x[i],
-				y: blocks.y[i],
-				color: tileColor,
-				size: tileSize
-			});
-		}
+    for (var i = 4; i--;) {
+      this.blockStack.push({
+        x: blocks.x[i],
+        y: blocks.y[i],
+        color: tileColor,
+        size: tileSize
+      });
+    }
 
-		boardCanvas.clear();
+    boardCanvas.clear();
 
-		for (var j = blockStack.length; j--;) {
-			boardCanvas.renderTile(blockStack[j]);
-			if (blockStack[j].y < 0) {
-				overflows += 1;
-			}
-		}
+    for (var j = blockStack.length; j--;) {
+      boardCanvas.renderTile(blockStack[j]);
+      if (blockStack[j].y < 0) {
+        overflows += 1;
+      }
+    }
 
-		if (overflows) {
-			throw "Fail!!!";
-		}
+    if (overflows) {
+      throw "Fail!!!";
+    }
 
-	};
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.blockCheck = function(x, y) {
+  GameBoard.prototype.blockCheck = function(x, y) {
 
-		var blockStack = this.blockStack;
+    var blockStack = this.blockStack;
 
-		for (var i = blockStack.length; i--;) {
-			if (blockStack[i].x === x && blockStack[i].y === y) {
-				return true;
-			}
-		}
+    for (var i = blockStack.length; i--;) {
+      if (blockStack[i].x === x && blockStack[i].y === y) {
+        return true;
+      }
+    }
 
-	};
+  };
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	GameBoard.prototype.loop = function() {
+  GameBoard.prototype.loop = function() {
 
-		var self = this;
+    var self = this;
 
-		if (this.paused === false) {
+    if (this.paused === false) {
 
-			if ( ! this.currentPiece) {
-				this.createPiece();
-			}
+      if ( ! this.currentPiece) {
+        this.createPiece();
+      }
 
-			try {
-				this.movePiece("down");
-			} catch(e) {
-				this.paused = true;
-				alert(e);
-			}
+      try {
+        this.movePiece("down");
+      } catch(e) {
+        this.paused = true;
+        alert(e);
+      }
 
-		}
+    }
 
-		setTimeout(function() {
-			self.loop();
-		}, this.speed);
+    setTimeout(function() {
+      self.loop();
+    }, this.speed);
 
-	};
+  };
 
 
-	// ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
 
-	return GameBoard;
+  return GameBoard;
 
 });
